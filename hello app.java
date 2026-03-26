e5e95dad6edc44b2350c03117985cbd0ff9ac649
sg0093-247 <sg0093@srmist.edu.in>
2026-03-26 10:43:10 +0530

    Update hello app.java

--- hello app.java
+++ hello app.java
@@ -1,10 +1,21 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        
-        System.out.println("Hello World");
+        StringBuilder sb = new StringBuilder();
 
+        if (args.length == 0) {
+            sb.append("World");
+        } else {
+            int i = 0;
+            for (String name : args) {
+                sb.append(name);
+                if (i < args.length - 1) {
+                    sb.append(", ");
+                }
+                i++;
+            }
+        }
+
+        System.out.println("Hello, " + sb.toString() + "!");
     }
 }
-
